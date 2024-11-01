33575b77f4e8f6912ff8e8babddadcb55ae3356e
Atsuko Coffey <atsucoffey@gmail.com>
2024-11-01 20:10:12 +0000

    feat: Recipe model use  cloudinary and resized

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_resized import ResizedImageField
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """
+    Stores a single recipe entry related to :model:`auth.User`
+    """
+    title = models.CharField(max_length=100, null=False, blank=False)
+    description = models.CharField(max_length=400, null=False, blank=False)
+    ingredients = models.TextField(
+        max_length=10000, null=False, blank=False, default="Ingredients"
+    )
+    instructions = models.TextField(
+        max_length=10000, null=False, blank=False, default="Instructions"
+    )
+    image = ResizedImageField(
+        size=[400, None],
+        quality=75,
+        upload_to="recipes/",
+        force_format="WEBP",
+        blank=False,
+        null=False,
+    )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    posted_date = models.DateTimeField(auto_now=True)
+    lowsugar = models.BooleanField()
+    glutenfree = models.BooleanField()
+    dairyfree = models.BooleanField()
+    vegan = models.BooleanField()
+    vegitarian = models.BooleanField()
+    highfiber = models.BooleanField()
+    highprotein = models.BooleanField()
+    nutfree = models.BooleanField()
+
+    class Meta:
+        ordering = ["-posted_date"]
+
+    def __str__(self):
+        return str(self.title)
+
+    
+
