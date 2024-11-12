b3468e60ea1f75fdd397824e6807529599b6119c
Atsuko Coffey <atsucoffey@gmail.com>
2024-11-12 14:02:06 +0000

    feat: Add pagination and button - Home

--- home/views.py
+++ home/views.py
@@ -3,10 +3,15 @@ from recipes.models import Recipe
 
 
 class Index(ListView):
-    """View indexpage with recipes"""
+    """
+    Display the list of the recipes :model:`recipes.Recipe`.
+    **Template:**
+    :template:`home/index.html` 
+    """
     template_name = 'home/index.html'
     model = Recipe
     context_object_name = 'recipes'
+    paginate_by = 9
 
     def get_queryset(self):
         return self.model.objects.all()
