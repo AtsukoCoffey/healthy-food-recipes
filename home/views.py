3e8083a1198c805de7fda35c5a1113ccfcf8d09f
Atsuko Coffey <atsucoffey@gmail.com>
2024-11-12 23:38:59 +0000

    feat: Add search option modal - Home - only template and some functions

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.db.models import Q
 from recipes.models import Recipe
 
 
@@ -13,5 +14,28 @@ class Index(ListView):
     context_object_name = 'recipes'
     paginate_by = 9
 
-    def get_queryset(self):
-        return self.model.objects.all()
+    def get_queryset(self, **kwargs):
+        avoid_query = self.request.GET.get('q-avoid')
+        include_query = self.request.GET.get('q-incl')
+        owner_query = self.request.GET.get('q-owner')
+        free_query = self.request.GET.get('q-free')
+
+        if avoid_query:
+            recipes = self.model.objects.all().exclude(ingredients__contains=avoid_query)
+        elif include_query:
+            recipes = self.model.objects.filter(
+                Q(ingredients__icontains=include_query)
+            )
+        elif owner_query:
+            recipes = self.model.objects.filter(
+                Q(user__contains=owner_query)
+            )
+        elif free_query:
+            recipes = self.model.objects.filter(
+                Q(description__icontains=free_query) |
+                Q(instructions__icontains=free_query) |
+                Q(ingredients__icontains=free_query)
+            )
+        else:
+            recipes = self.model.objects.all()
+        return recipes
