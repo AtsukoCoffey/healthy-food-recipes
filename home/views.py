9ccd1b6154072571490c364143c6074edb87e925
Atsuko Coffey <atsucoffey@gmail.com>
2024-11-14 17:56:48 +0000

    refactor:Add search options - checkbox combinations

--- home/views.py
+++ home/views.py
@@ -15,65 +15,19 @@ class Index(ListView):
     paginate_by = 9
     recipes = Recipe.objects.all()
 
-    def get_queryset(self, **kwargs):
+    def get_queryset(self):
         """
         Search option modal's query
         8 checkboxes - on or None
         owner_query - int (foreign key)
         others are - string
         """
-        lowsugar = self.request.GET.get('lowsugar')
-        glutenfree = self.request.GET.get('glutenfree')
-        dairyfree = self.request.GET.get('dairyfree')
-        vegan = self.request.GET.get('vegan')
-        vegitarian = self.request.GET.get('vegitarian')
-        highfiber = self.request.GET.get('highfiber')
-        highprotein = self.request.GET.get('highprotein')
-        nutfree = self.request.GET.get('nutfree')
-        avoid_query = self.request.GET.get('q-avoid')
-        include_query = self.request.GET.get('q-incl')
-        owner_query = self.request.GET.get('q-owner')
-        free_query = self.request.GET.get('q-free')
+        queryset = Recipe.objects.all()
+        filters = {}
+        for field in ['lowsugar', 'glutenfree', 'dairyfree', 'vegan', 'vegitarian', 'highfiber', 'highprotein', 'nutfree']:
+            if self.request.GET.get(field):
+                filters[field] = True
+        if filters:
+            queryset = queryset.filter(**filters)
         
-        def lowsugar():
-            recipes = recipes.filter(lowsugar=True)
-        
-        def glutenfree():
-            recipes = recipes.filter(glutenfree=True)
-        
-        def dairyfree():
-            recipes = recipes.filter(dairyfree=True)
-        
-        def vegan():
-            recipes = recipes.filter(vegan=True)
-        
-        def vegitarian():
-            recipes = recipes.filter(vegitarian=True)
-        
-        def highfiber():
-            recipes = recipes.filter(highfiber=True)
-        
-        def highprotein():
-            recipes = recipes.filter(highprotein=True)
-        
-        def nutfree():
-            recipes = recipes.filter(nutfree=True)
- 
-        if lowsugar:
-            lowsugar()
-        if glutenfree:
-            glutenfree()
-        if dairyfree:
-            dairyfree()
-        if vegan:
-            vegan()
-        if vegitarian:
-            vegitarian()
-        if highfiber:
-            highfiber()
-        if highprotein:
-            highprotein()
-        if nutfree:
-            nutfree()
-        
-        return recipes
+        return queryset
