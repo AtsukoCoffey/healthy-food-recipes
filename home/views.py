a28b1944598ea7ac7c7750164606af6294212cc6
Atsuko Coffey <atsucoffey@gmail.com>
2024-11-19 17:39:01 +0000

    fix: Search result's pagination issue solved - tutor assistant

--- home/views.py
+++ home/views.py
@@ -19,10 +19,17 @@ class Index(ListView):
     context_object_name = 'recipes'
     paginate_by = 9
     queryset = Recipe.objects.all()
+    print('ASSIGNED QUERYSET: ', queryset)
 
     # For owner search, send all the User model's name and pk
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Add GET parameters to context
+        query_params = self.request.GET.copy()
+        if 'page' in query_params:
+            query_params.pop('page') 
+        context['query_string'] = query_params.urlencode()
+        # All user's data
         context['all_users'] = User.objects.all()
         return context
 
@@ -39,6 +46,7 @@ class Index(ListView):
         free_query = self.request.GET.get('q-free')
 
         queryset = Recipe.objects.all()
+        print('REDEFINED QUERYSET: ', queryset)
         
         # 8 checkboxs all the combination query
         # Save the "on" checkbox field in `filters` dictionary
@@ -48,7 +56,9 @@ class Index(ListView):
                 filters[field] = True
         # The filter dictionary goes in the kwargs as like `lowsugar=True` 
         if filters:
+            print('FILTERS: ', filters)
             queryset = queryset.filter(**filters)
+            print('FILTERED QUERYSET: ', queryset)
         ## pagination issue ========================================================> address later
 
         # Avoid and include Ingredients query
@@ -72,9 +82,11 @@ class Index(ListView):
                 Q(description__icontains=free_query) |
                 Q(instructions__icontains=free_query)
             )
+            print('FREE QUERYSET: ', queryset)
 
         # Owner search 
         if owner_query:
             queryset = queryset.filter(Q(user=owner_query))
         
+        print('BEFORE RETURN: ', queryset)
         return queryset
